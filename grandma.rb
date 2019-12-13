c0fdaccb4d95a30548846b89f28ca6131a2ab285
Kyshon Gary <kgarywork21@gmail.com>
2019-12-13 00:46:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(string)
 
   if string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
     elsif string == string.upcase
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else 
     "HUH?! SPEAK UP, SONNY!"
   end
